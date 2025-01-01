912922a936f99a42ef326a3c7f75f657f2b9cb15
Rana718 <ranadolui718@gmail.com>
2025-01-01 13:14:15 +0530

    new app with change some config

--- App.tsx
+++ App.tsx
@@ -77,8 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App.tsx</Text> i Love you Diya 💕💕❤️
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
