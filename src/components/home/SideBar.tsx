8cb76f8661e94fe94932636c860b862e2e96b82d
Rana718 <ranadolui718@gmail.com>
2025-01-29 19:28:00 +0530

    remove the side bar

---
+++ src/components/home/SideBar.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { View, Text, TouchableOpacity } from 'react-native';
+// import Icons from '../global/Icons';
+// import { Colors } from '../../utils/Constants';
+
+// const SideBar = () => {
+//     return (
+//         <View className='flex-1 bg-white p-5'>
+//             <TouchableOpacity className='flex-row items-center mb-5' >
+//                 {/* <Icons iconFamily='FontAwesome' color='black' size={24} name='circle-user'/> */}
+//                 <Text className='ml-[10px] text-xl'>Profile</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity className='flex-row items-center'>
+//                 <Icons name='qrcode' iconFamily='MaterialCommunityIcons' color={Colors.primary} size={16}/>
+//                 <Text className='ml-[10px] text-xl'>Show your Qrcode</Text>
+//             </TouchableOpacity>
+//         </View>
+//     );
+// };
+
+// export default SideBar;
